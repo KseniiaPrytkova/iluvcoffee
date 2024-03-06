a92e387dd31fde86eb413337053b2c79edbb9822
kseniiaprytkova-hn <kseniia.prytkova@hn-company.co.uk>
2024-03-06 15:22:56 +0000

    leverage async providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,14 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity/event.entity';
+import { Connection } from 'typeorm';
 
-@Injectable()
-export class CoffeeBrandsFactory {
-  create() {
-    // do smth
-    return ['buddy brew', 'nescafe'];
-  }
-}
+// @Injectable()
+// export class CoffeeBrandsFactory {
+//   create() {
+//     // do smth
+//     return ['buddy brew', 'nescafe'];
+//   }
+// }
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
@@ -20,12 +21,18 @@ export class CoffeeBrandsFactory {
   // providers: [CoffeesService],
   providers: [
     CoffeesService,
-    CoffeeBrandsFactory,
     {
       provide: 'COFFEE_BRANDS',
-      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
-        brandsFactory.create(),
-      inject: [CoffeeBrandsFactory],
+      // Note "async" here, and Promise/Async event inside the Factory function
+      // Could be a database connection / API call / etc
+      // In our case we're just "mocking" this type of event with a Promise
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        // const coffeeBrands = await connection.query('SELECT * ...');
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+        console.log('[!] Async Factory');
+        return coffeeBrands;
+      },
+      inject: [Connection],
     },
   ],
   exports: [CoffeesService],
