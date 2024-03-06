e698820b35bd3ae9bcd7a6805feb780f7f6127e5
kseniiaprytkova-hn <kseniia.prytkova@hn-company.co.uk>
2024-03-06 14:56:15 +0000

    factory providers: realistic example

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,15 +6,26 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity/event.entity';
 
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    // do smth
+    return ['buddy brew', 'nescafe'];
+  }
+}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   // providers: [CoffeesService],
   providers: [
     CoffeesService,
+    CoffeeBrandsFactory,
     {
       provide: 'COFFEE_BRANDS',
-      useFactory: () => ['buddy brew', 'nescafe'],
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
     },
   ],
   exports: [CoffeesService],
